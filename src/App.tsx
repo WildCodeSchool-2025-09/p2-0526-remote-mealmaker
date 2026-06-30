dc3cb61bf327015cd8db36ead77a1af8d84c9d18
Laurent Koehler <laurent.koehler@gmail.com>
2026-06-30 10:36:40 +0200

    fix(US00): create Router.tsx and fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Recipe from "./pages/Recipe";
-import RecipeLive from "./pages/RecipeLive";
-import Favorite from "./pages/Favorite";
-import ThemePreview from "./pages/ThemePreview";
 import Navbar from "./components/NavBar";
+import Router from "./Router";
 
 function App() {
 	return (
 		<>
 			<Navbar />
-
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/recipe/:id" element={<Recipe />} />
-				<Route path="/recipelive" element={<RecipeLive />} />
-				<Route path="/favorite" element={<Favorite />} />
-				<Route path="/themepreview" element={<ThemePreview />} />
-			</Routes>
+			<Router />
 		</>
 	);
 }
