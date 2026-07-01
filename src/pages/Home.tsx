15885cec8b20691762aff284df1e331813beec53
Guillaume Galinanes <galinanesgm@gmail.com>
2026-07-01 12:37:33 +0200

    feat(US01): Add search CSS

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Menu, Search } from "lucide-react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
@@ -6,6 +7,19 @@ function Home() {
 		<>
 			<Header />
 			<Hero />
+			<section className="w-full flex justify-center items-center gap-2 mt-4 px-4">
+				<label className="input bg-surface border border-solid border-primary">
+					<Search />
+					<input
+						type="search"
+						required
+						placeholder="Recherche un ingrédient..."
+						className="input"
+					/>
+				</label>
+
+				<Menu />
+			</section>
 		</>
 	);
 }
