20666fbd511a78519563f3b9f28ca09c3519af52
Laurent Koehler <laurent.koehler@gmail.com>
2026-07-03 10:39:24 +0200

    fix(US02): Import Hamburger in Recipe.tsx

--- src/pages/Recipe.tsx
+++ src/pages/Recipe.tsx
@@ -1,7 +1,17 @@
 import { useParams } from "react-router";
 import Navbar from "../components/NavBar";
 import Header from "../components/Header";
-import { Apple, ArrowLeft, ChefHat, Drumstick, Heart, Popcorn, Star, Timer } from "lucide-react";
+import {
+	Apple,
+	ArrowLeft,
+	ChefHat,
+	Drumstick,
+	Hamburger,
+	Heart,
+	Popcorn,
+	Star,
+	Timer,
+} from "lucide-react";
 
 function Recipe() {
 	const { id } = useParams();
@@ -10,14 +20,25 @@ function Recipe() {
 		<>
 			<Header />
 			<section className="w-full relative">
-				<img src="https://picsum.photos/id/237/900/600" alt="Test" className="w-full" />
+				<img
+					src="https://picsum.photos/id/237/900/600"
+					alt="Test"
+					className="w-full"
+				/>
 				<ul className="w-full h-24 absolute top-0 flex justify-between p-4">
-					<li className="text-surface"><ArrowLeft /></li>
-					<li className="text-alert"> <Heart /></li>
+					<li className="text-surface">
+						<ArrowLeft />
+					</li>
+					<li className="text-alert">
+						{" "}
+						<Heart />
+					</li>
 				</ul>
 			</section>
 			<section className="w-full">
-				<h1 className="flex text-xl font-bold pl-4 text-text py-4">Nom de la recette</h1>
+				<h1 className="flex text-xl font-bold pl-4 text-text py-4">
+					Nom de la recette
+				</h1>
 				<ul className="w-full flex text-sm">
 					<li className="flex text-secondary items-center px-4">
 						<Timer />
@@ -42,43 +63,47 @@ function Recipe() {
 						<a href="#item2">Ingrédients</a>
 					</li>
 					<li className="min-h-8 text-center font-bold">
-						<a href="#item3"> Etapes
-						</a>
+						<a href="#item3"> Etapes</a>
 					</li>
 				</ul>
 				<article className="carousel w-full">
 					<p id="item1" className="carousel-item w-full">
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-						orem Ipsum has been the industry's standard dummy text ever since 1966,
-						when designers at Letraset and James Mosley, the librarian at St Bride
-						Printing Library in London,
+						Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry. orem Ipsum has been the industry's standard dummy text
+						ever since 1966, when designers at Letraset and James Mosley, the
+						librarian at St Bride Printing Library in London,
 					</p>
 					<p id="item2" className="carousel-item w-full">
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-						orem Ipsum has been the industry's standard dummy text ever since 1966,
-						when designers at Letraset and James Mosley, the librarian at St Bride
-						Printing Library in London,
+						Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry. orem Ipsum has been the industry's standard dummy text
+						ever since 1966, when designers at Letraset and James Mosley, the
+						librarian at St Bride Printing Library in London,
 					</p>
 					<p id="item3" className="carousel-item w-full">
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-						orem Ipsum has been the industry's standard dummy text ever since 1966,
-						when designers at Letraset and James Mosley, the librarian at St Bride
-						Printing Library in London,
+						Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry. orem Ipsum has been the industry's standard dummy text
+						ever since 1966, when designers at Letraset and James Mosley, the
+						librarian at St Bride Printing Library in London,
 					</p>
 				</article>
 			</section>
 			<section>
 				<h2>Ingrédients principaux</h2>
 				<ul>
-					<li><Drumstick /></li>
-					<li><Popcorn /></li>
-					<li><Apple /></li>
-					<li><Hamburger /></li>
+					<li>
+						<Drumstick />
+					</li>
+					<li>
+						<Popcorn />
+					</li>
+					<li>
+						<Apple />
+					</li>
+					<li>
+						<Hamburger />
+					</li>
 				</ul>
 			</section>
-
-
-
 
 			{/* <button className=" w-full flex justify-center rounded-sm text-surface bg-brand-hover ">Lancer la recette</button> */}
 
