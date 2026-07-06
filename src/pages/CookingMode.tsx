70fe15fad041b2120bb3551992e10d1b4ced864f
Laurent Koehler <laurent.koehler@gmail.com>
2026-07-06 12:33:32 +0200

    feat(US04): add fetch API to CookingMode

--- src/pages/CookingMode.tsx
+++ src/pages/CookingMode.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+
+type RecipeStep = {
+	number: number;
+	step: string;
+	length?: { number: number; unit: string };
+};
+type CookingRecipe = {
+	title: string;
+	analyzedInstructions: { steps: RecipeStep[] }[];
+};
 
 function CookingMode() {
 	const { id } = useParams();
+	const [recipe, setRecipe] = useState<CookingRecipe>();
+
+	useEffect(() => {
+		const myApiKey = import.meta.env.VITE_API_URL;
+
+		fetch(
+			`https://api.spoonacular.com/recipes/${id}/information?apiKey=${myApiKey}`,
+		)
+			.then((response) => response.json())
+			.then((data) => setRecipe(data));
+	}, [id]);
 
 	return (
 		<>
