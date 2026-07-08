478f22e976a9b81ec0a100188c2d6dc8635467c9
alexandravelisar <anda_alexandra91@yahoo.com>
2026-07-08 14:16:08 +0200

    fix(US03):add close menu on click outside filter menu

--- src/components/FilterMenu.tsx
+++ src/components/FilterMenu.tsx
@@ -1,12 +1,32 @@
 import { Menu } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function FilterMenu({ filters, setFilters }) {
 	const [isOpen, setIsOpen] = useState(false);
+	const menuRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		function handleClickOutside(event: MouseEvent) {
+			if (
+				menuRef.current &&
+				!menuRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		}
+
+		document.addEventListener("mousedown", handleClickOutside);
+
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, []);
 
 	return (
 		<>
-			<div className="dropdown dropdown-end">
+			<div
+				ref={menuRef}
+				className="dropdown dropdown-end">
 				<button
 					type="button"
 					className="btn"
@@ -16,7 +36,10 @@ function FilterMenu({ filters, setFilters }) {
 				</button>
 				{isOpen && (
 					<div>
-						<ul className="absolute right-0 mt-2 w-72 bg-surface border border-primary/20 rounded-box shadow-xl p-5 z-50 space-y-4">
+						<ul className=" absolute
+    right-0 mt-2 w-[90vw] sm:w-80 max-w-sm p-4 sm:p-5 bg-surface
+    border border-primary/20 rounded-box shadow-xl
+    z-5 space-y-4">
 							<h3 className="font-heading text-lg text-primary font-semibold">
 								Filtres
 							</h3>
@@ -105,15 +128,24 @@ function FilterMenu({ filters, setFilters }) {
 								</select>
 							</li>
 							<li>
-								<div className="flex justify-center gap-3 pt-2">
+								<div className="flex flex-col sm:flex-row gap-3 pt-2">
 									<button
 										type="button"
-										className="btn btn-outline btn-secondary"
+										className="btn btn-outline btn-secondary" onClick={() => {
+											setFilters({
+												vegan: false,
+												healthy: false,
+												sport: false,
+												diet: "",
+												intolerances: "",
+											});
+										}}
 									>
 										Reset
 									</button>
 
-									<button type="button" className="btn btn-secondary">
+									<button type="button" className="btn btn-secondary"
+										onClick={() => setIsOpen(false)}>
 										Apply
 									</button>
 								</div>
