e9fdadc7de1ea19961ff4c725a13727dc09ff5da
Laurent Koehler <laurent.koehler@gmail.com>
2026-07-14 16:13:12 +0200

    feat(US06): Favorite Page

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -1,5 +1,31 @@
+import { useFavorite } from "../components/contexts/FavoriteContext";
+import Navbar from "../components/NavBar";
+import RecipeCard from "../components/RecipeCard";
+
 function Favorite() {
-	return <h1>Hello from Favorite</h1>;
+	const { favorites } = useFavorite();
+
+	if (favorites.length === 0) {
+		return (
+			<>
+				<p className="text-center text-text-muted mt-10">
+					Aucune recette favorite pour l'instant.
+				</p>
+				<Navbar />
+			</>
+		);
+	}
+
+	return (
+		<>
+			<section className="flex flex-col p-8 gap-4">
+				{favorites.map((recipe) => (
+					<RecipeCard key={recipe.id} recipe={recipe} />
+				))}
+			</section>
+			<Navbar />
+		</>
+	);
 }
 
 export default Favorite;
