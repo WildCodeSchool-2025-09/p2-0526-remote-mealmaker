b078e84c7de2069b77370ea02c88ef678a18014c
Laurent Koehler <laurent.koehler@gmail.com>
2026-07-14 15:41:09 +0200

    feat(USO6): Favorite button on RecipeCard and HeroRecipePage, + refacto

--- src/components/contexts/FavoriteContext.tsx
+++ src/components/contexts/FavoriteContext.tsx
@@ -1,11 +1,27 @@
 import { createContext, useContext, useState } from "react";
+import type { Recipe } from "../../types/recipe.types";
 
 const FavoriteContext = createContext(null);
 
 export function FavoriteProvider({ children }) {
-	const [favorite, setFavorite] = useState(false);
+	const [favorites, setFavorites] = useState<Recipe[]>([]);
+
+	const isFavorite = (recipeId: number) => {
+		return favorites.some((favorite) => favorite.id === recipeId);
+	};
+
+	const toggleFavorite = (recipe: Recipe) => {
+		const alreadyFavorite = isFavorite(recipe.id);
+
+		if (alreadyFavorite) {
+			setFavorites(favorites.filter((favorite) => favorite.id !== recipe.id));
+		} else {
+			setFavorites([...favorites, recipe]);
+		}
+	};
+
 	return (
-		<FavoriteContext value={{ favorite, setFavorite }}>
+		<FavoriteContext value={{ favorites, toggleFavorite, isFavorite }}>
 			{children}
 		</FavoriteContext>
 	);
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { FavoriteProvider } from "./components/context/FavoriteContext";
+import { FavoriteProvider } from "./components/contexts/FavoriteContext";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
